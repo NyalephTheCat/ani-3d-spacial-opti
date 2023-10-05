67de3d16acf5dd3adb87602695971b2b511a8bde
Chloe Magnier <contact@nyaleph.com>
2023-10-06 00:36:32 +0200

    todo: fix force

--- src/simulation/simulation.cpp
+++ src/simulation/simulation.cpp
@@ -33,7 +33,10 @@ void update_density(Grid2d &grid, sph_parameters_structure const& sph_parameters
     for (auto particle: grid.get_all_particles()) {
         particle->rho = 0.0f;
 
-        for (auto neighbour: grid.get_particles_influencing(*particle)) {
+        for (auto neighbour: grid.get_all_particles()) {
+            if (norm(particle->p - neighbour->p) > h) {
+                continue;
+            }
             particle->rho += m * W_density(particle->p, neighbour->p, h);
         }
     }
